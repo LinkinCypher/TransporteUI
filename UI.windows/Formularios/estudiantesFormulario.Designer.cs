923d4e3d088290ff507295706438872f796bb442
LinkinCypher <linkincypher@hotmail.com>
2024-01-21 08:00:49 -0500

    form estudiantes

--- UI.windows/Formularios/estudiantesFormulario.Designer.cs
+++ UI.windows/Formularios/estudiantesFormulario.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dg_estudiantes = new System.Windows.Forms.DataGridView();
             this.bt_guardar = new System.Windows.Forms.Button();
             this.txt_especialidad = new System.Windows.Forms.TextBox();
@@ -55,7 +56,16 @@
             // 
             this.dg_estudiantes.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dg_estudiantes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dg_estudiantes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dg_estudiantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_estudiantes.EnableHeadersVisualStyles = false;
             this.dg_estudiantes.Location = new System.Drawing.Point(369, 50);
             this.dg_estudiantes.Name = "dg_estudiantes";
             this.dg_estudiantes.Size = new System.Drawing.Size(844, 388);
